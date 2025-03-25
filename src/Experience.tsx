f9f6e3c43550be63b8a3d9e3dc90c4963d240209
tigeryash <yashwanth.venkatesan@gmail.com>
2025-03-25 00:08:25 -0400

    Add shader materials and update background color in Experience component

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,12 +1,24 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import * as THREE from "three";
+
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
 
 function Experience() {
+  const key = THREE.MathUtils.generateUUID();
   return (
     <>
       <Perf position="top-left" />
       <OrbitControls />
-
+      <mesh>
+        <icosahedronGeometry args={[1, 5]} />
+        <shaderMaterial
+          key={key}
+          vertexShader={vertexShader}
+          fragmentShader={fragmentShader}
+        />
+      </mesh>
       <ambientLight intensity={0.2} />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
         far: 200,
       }}
     >
+      <color attach="background" args={["#000000"]} />
       <Experience />
     </Canvas>
     <Leva collapsed />
