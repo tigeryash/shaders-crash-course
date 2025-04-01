d8a2be1c087251f141b3ca1a33472a4875514f3d
Yash <yashwanth.venkatesan@gmail.com>
2025-04-01 02:29:53 -0400

    Refactor shader implementation and update geometry in Experience component

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -2,29 +2,39 @@ import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import * as THREE from "three";
 
-import vertexShader from "./shaders/vertex.glsl";
-import fragmentShader from "./shaders/fragment.glsl";
-import { useControls } from "leva";
-import magic from "./assets/magic.png";
+import vertexShader from "./shaders/final/vertex.glsl";
+import fragmentShader from "./shaders/final/fragment.glsl";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+// import { useControls } from "leva";
+// import magic from "./assets/magic.png";
 
 function Experience() {
   const key = THREE.MathUtils.generateUUID();
-  const { uRadius } = useControls("Shader", {
-    uRadius: { value: 0.2, min: 0, max: 1, step: 0.01 },
+  const matRef = useRef<THREE.ShaderMaterial | null>(null);
+  // const { uRadius } = useControls("Shader", {
+  //   uRadius: { value: 0.2, min: 0, max: 1, step: 0.01 },
+  // });
+  useFrame((_, delta) => {
+    if (matRef.current) {
+      matRef.current.uniforms.uTime.value += delta;
+    }
   });
   return (
     <>
       <Perf position="top-left" />
       <OrbitControls />
       <mesh>
-        <planeGeometry args={[2, 2]} />
+        <icosahedronGeometry args={[1, 100]} />
         <shaderMaterial
           key={key}
+          ref={matRef}
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
           uniforms={{
-            uRadius: { value: uRadius },
-            uTexture: { value: new THREE.TextureLoader().load(magic) },
+            uTime: { value: 0 },
+            // uRadius: { value: uRadius },
+            // uTexture: { value: new THREE.TextureLoader().load(magic) },
           }}
         />
       </mesh>
