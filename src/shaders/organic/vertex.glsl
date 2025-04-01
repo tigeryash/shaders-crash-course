761f8b1bc9c6f8264501dc94aa4a899ec83aaac9
Yash <yashwanth.venkatesan@gmail.com>
2025-04-01 02:31:05 -0400

    Remove organic shader fragment and vertex shader files

--- src/shaders/organic/vertex.glsl
+++
@@ -1,8 +0,0 @@
-varying vec2 vUv;
-void main(){
-    vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-    vec4 projectedPosition = projectionMatrix * modelViewPosition;
-    gl_Position = projectedPosition;
-
-    vUv = uv;
-}
